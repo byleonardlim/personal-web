4ec9c687f6be9b7e01ec6e62e4b63aaa43d5273e
Leonard Lim <korett@NombyP14S.>
2025-03-31 15:11:16 +0800

    font fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="antialiased">
+      <body className="text-gray-900">
         <Main />
         <NextScript />
       </body>
