d7ec47cfcd413f12d2541596aadb033c4cd8131a
Leonard Lim <korett@NombyP14S.>
2025-03-31 13:57:13 +0800

    digital ocean fixes

--- src/pages/case-study/[slug].tsx
+++ src/pages/case-study/[slug].tsx
@@ -236,11 +236,11 @@ const SectionedMarkdown: React.FC<SectionedMarkdownProps> = ({ content }) => {
           <div className="lg:w-1/3 lg:my-12 order-first lg:order-last">
             <div className="sticky lg:top-8 border border-gray-100 lg:rounded-lg p-6 bg-gray-50">
               <h2 className="text-lg font-bold mb-4 text-gray-700 uppercase">
-                {notesSection.heading}
+                {notesSection[heading]}
               </h2>
               <div className="text-sm text-gray-600 leading-relaxed">
                 <ReactMarkdown components={MarkdownComponents}>
-                  {notesSection.content}
+                  {notesSection[content]}
                 </ReactMarkdown>
               </div>
             </div>
